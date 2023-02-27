927cf314c5f032d49df7436d4c3d3dbffa565f5f
K1Nf <nikitka.repin.2017@mail.ru>
2023-02-27 13:58:13 +0500

    added describe

--- proj1.js
+++ proj1.js
@@ -2,3 +2,60 @@
 //Ускорение видео (эмблема ЮграТех)
 document.querySelector('video').playbackRate = 2.0;
 
+function viewDiv1(){
+
+	display = document.getElementById("div1").style.display;
+
+    if(display=='none')
+    {
+       document.getElementById("div1").style.display = "block";
+    }
+    else
+    {
+       document.getElementById("div1").style.display = 'none';
+    }
+
+    //document.getElementById("div1").style.display = "block";
+};
+
+function viewDiv2(){
+
+    display = document.getElementById("div2").style.display;
+
+    if(display=='none')
+    {
+       document.getElementById("div2").style.display = "block";
+    }
+    else
+    {
+       document.getElementById("div2").style.display = 'none';
+    }
+};
+
+function viewDiv3(){
+
+    display = document.getElementById("div3").style.display;
+
+    if(display=='none')
+    {
+       document.getElementById("div3").style.display = "block";
+    }
+    else
+    {
+       document.getElementById("div3").style.display = 'none';
+    }
+};
+
+function viewDiv4(){
+
+    display = document.getElementById("div4").style.display;
+
+    if(display=='none')
+    {
+       document.getElementById("div4").style.display = "block";
+    }
+    else
+    {
+       document.getElementById("div4").style.display = 'none';
+    }
+};
